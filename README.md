7cce7c03ac132e697f92f601738936f28d6ceee3
akhilvc10 <akhilvc10@gmail.com>
2021-04-29 22:33:48 +0530

    added storybook

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # hover-design-system
+
+### Getting Started
+
+  Run storybook - `npm run storybook`
---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react";
+import { withDesign } from "storybook-addon-designs";
+
+import { Button, ButtonProps } from "./Button";
+
+export default {
+	title: "Example/Button",
+	component: Button,
+	decorators: [withDesign],
+	argTypes: {
+		backgroundColor: { control: "color" }
+	}
+} as Meta;
+
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+	accent: "primary",
+	label: "Button"
+};
+
+Primary.parameters = {
+	design: {
+		type: "figma",
+		url:
+			"https://www.figma.com/file/cOnIxpJzyQUUrcN8flCymY/Untitled?node-id=1%3A2",
+		allowFullscreen: true
+	}
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+	accent: "secondary",
+	label: "Button"
+};
+
+export const Success = Template.bind({});
+Success.args = {
+	accent: "success",
+	label: "Button"
+};
+
+export const Large = Template.bind({});
+Large.args = {
+	size: "large",
+	label: "Button"
+};
+
+export const Small = Template.bind({});
+Small.args = {
+	size: "small",
+	label: "Button"
+};
+
+export const XLarge = Template.bind({});
+XLarge.args = {
+	size: "xtraLarge",
+	label: "Button"
+};
