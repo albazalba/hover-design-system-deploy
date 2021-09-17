9806dd1f66e082e7e66ca2dbe3d5d51b308d115a
albaz alba <you@example.com>
2021-09-17 15:15:15 +0530

    updated list and dropdown components

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,6 @@ import {FiMoreVertical} from 'react-icons/fi'
 function App() {
   return (
     <div className="app">
-      {/* <Button label="Click me" accent="success" rounded /> */}
-      <DropDown
-					button={<FiMoreVertical />}
-					buttonStyle={{border:"none", height:"45px", width:"45px", borderRadius:"50%", margin:"5px" }}
-					list={[
-						{ label: 'Frenchy', onSelect: () => console.log("hiii")},
-						{ label: 'Coco', },
-						{ label: 'Kit-Kat' },
-						{ label: 'Lei' },
-						{ label: 'Ankith' }
-					]}
-					/>
     </div>
   );
 }
